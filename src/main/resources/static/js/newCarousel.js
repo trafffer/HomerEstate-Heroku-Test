917c287aa93a475c560e4c15a177eb26d1178143
trafffer <sonya0@mail.bg>
2021-05-31 00:02:11 +0300

    Added functionality to shift by dragging with mouse. Fixed issue with improper carousel shifting.

--- src/main/resources/static/js/newCarousel.js
+++ src/main/resources/static/js/newCarousel.js
@@ -3,11 +3,15 @@ const slides = Array.from(document.getElementsByClassName('carousell__slide'));
 const indicator = document.querySelector('.carousell__nav')
 const dots = Array.from(indicator.children);
 
+
 window.onload = function () {
     slides.forEach(setSlidePosition);
 };
 
 function setSlidePosition(slide,index){
+    if (slides.length<3){
+        track.style.justifyContent = 'center';
+    }
     let width = 0;
     for (let i=1;i<=index;i++){
         width+=slideLeft(i-1);
@@ -16,7 +20,7 @@ function setSlidePosition(slide,index){
 }
 
 function slideLeft(index) {
-    return slides[index].clientWidth+20;
+    return slides[index].clientWidth +20;
 }
 
 
@@ -50,5 +54,43 @@ indicator.addEventListener('click',e=>{
     updateDots(currentDot,targetDot);
 })
 
+let isDown = false;
+let startX;
+let endX;
+let walk;
 
+track.addEventListener('mousedown',evt => {
+    isDown= true;
+    startX = evt.pageX - track.offsetLeft;
+})
 
+track.addEventListener('mouseleave',(e) => {
+    isDown = false;
+})
+
+track.addEventListener('mouseup',(e) => {
+    isDown = false;
+})
+
+track.addEventListener('mousemove',evt => {
+    if (!isDown||slides.length<2){return;}
+    const currentSlide = track.querySelector('.active');
+    const currentDot = indicator.querySelector('.current-dot');
+    const index = slides.findIndex(e=>e.className.endsWith('active'));
+    let x;
+    evt.preventDefault();
+    endX = evt.pageX - track.offsetLeft;
+    walk = endX-startX;
+    if (walk<0){
+        x = index+1;
+    }else{
+        x = index-1;
+    }
+    if (x>=0||x<slides.length) {
+        let targetSlide = slides[x];
+        let targetDot = dots[x];
+        moveToSlide(track, currentSlide, targetSlide);
+        updateDots(currentDot, targetDot);
+    }
+    isDown = false;
+})
