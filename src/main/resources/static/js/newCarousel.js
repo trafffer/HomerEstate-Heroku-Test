d55c4ca72922ab1787be146dfdff3501ea22f0da
trafffer <sonya0@mail.bg>
2021-05-31 01:59:06 +0300

    Make the carousel working for mobile device.

--- src/main/resources/static/js/newCarousel.js
+++ src/main/resources/static/js/newCarousel.js
@@ -59,38 +59,46 @@ let startX;
 let endX;
 let walk;
 
-track.addEventListener('mousedown',evt => {
+const gestureStart = (e) => {
     isDown= true;
-    startX = evt.pageX - track.offsetLeft;
-})
+    startX = e.pageX - track.offsetLeft;
+}
 
-track.addEventListener('mouseleave',(e) => {
-    isDown = false;
-})
-
-track.addEventListener('mouseup',(e) => {
-    isDown = false;
-})
-
-track.addEventListener('mousemove',evt => {
+const gestureMove = (e) => {
     if (!isDown||slides.length<2){return;}
     const currentSlide = track.querySelector('.active');
     const currentDot = indicator.querySelector('.current-dot');
     const index = slides.findIndex(e=>e.className.endsWith('active'));
     let x;
-    evt.preventDefault();
-    endX = evt.pageX - track.offsetLeft;
+    e.preventDefault();
+    endX = e.pageX - track.offsetLeft;
     walk = endX-startX;
     if (walk<0){
         x = index+1;
     }else{
         x = index-1;
     }
-    if (x>=0||x<slides.length) {
+    if (x>=0&&x<slides.length) {
         let targetSlide = slides[x];
         let targetDot = dots[x];
         moveToSlide(track, currentSlide, targetSlide);
         updateDots(currentDot, targetDot);
     }
     isDown = false;
-})
+}
+const gestureEnd = (e)=>{
+    isDown = false;
+}
+
+if (window.PointerEvent){
+    track.addEventListener('pointerdown',gestureStart);
+    track.addEventListener('pointerup',gestureEnd);
+    track.addEventListener('pointermove',gestureMove);
+}else{
+    track.addEventListener('touchdown',gestureStart);
+    track.addEventListener('touchup',gestureEnd);
+    track.addEventListener('touchmove',gestureMove);
+    track.addEventListener('mousedown',gestureStart);
+    track.addEventListener('mouseup',gestureEnd);
+    track.addEventListener('mousemove',gestureMove);
+}
